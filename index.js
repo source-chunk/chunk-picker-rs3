7ac130862025ab8d935b75d88c4d1880c8a778e4
source chunk <duosource.iron@gmail.com>
2024-04-19 09:50:59 -0600

    6.1.13 - Added agility section connections

--- index.js
+++ index.js
@@ -846,7 +846,7 @@ let settingNames = {
     "startingChunk": "Starting Chunk",
     "numTasksPercent": "Show Active Task number as a percentage instead of a fraction",
     "newTasks": "In addition to adding new tasks to the Active Chunk Tasks list, also show new chunk tasks in a popup window after every chunk roll",
-    "shiftUnlock": "Prevent click-to-unlock chunks unless holding down the Shift-key",
+    "shiftUnlock": "Prevent click-to-unlock chunks unless holding down the Shift-key (or long-pressing on mobile)",
     "rollWarning": "Show a confirmation window after clicking the Pick Chunk button",
     "optOutSections": "Always assume all chunks are entirely accessible (opt out of chunk sections)",
 };                                                                              // Descriptions of the settings
@@ -1300,8 +1300,9 @@ let chunkSectionCalculateAfter = false;
 let signInAttempts = 0;
 let expandChallengeStr = '';
 let detailsStack = [];
+let touchTime = 0;
 
-let currentVersion = '6.1.12';
+let currentVersion = '6.1.13';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1411,7 +1412,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "runescape_world_map.png?v=6.1.12";
+mapImg.src = "runescape_world_map.png?v=6.1.13";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -1958,6 +1959,9 @@ let handleMouseDown = function(e) {
     }
     mouseDown = true;
     movedNum = 0;
+    if (e.type === 'touchstart') {
+        touchTime = Date.now();
+    }
 }
 
 // Handles mouse move event
@@ -2175,7 +2179,7 @@ let handleMouseUp = function(e) {
             } else if (isHoveringSticker) {
                 openStickers(chunkId);
                 return;
-            } else if (settings['shiftUnlock'] && !e.shiftKey && (!testMode || !locked) && !onMobile) {
+            } else if (settings['shiftUnlock'] && !e.shiftKey && (!testMode || !locked) && (!onMobile || (Date.now() - touchTime) < 500)) {
                 return;
             } else if (!!tempChunks['unlocked'] && tempChunks['unlocked'].hasOwnProperty(chunkId)) {
                 if (!recentChunks.hasOwnProperty(chunkId)) {
@@ -2839,7 +2843,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.1.12");
+        myWorker = new Worker("./worker.js?v=6.1.13");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], mid === manualAreasOnly, tempSections, settings['optOutSections']]);
         workerOut = 1;
@@ -3101,8 +3105,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.1.12");
-let myWorker2 = new Worker("./worker.js?v=6.1.12");
+let myWorker = new Worker("./worker.js?v=6.1.13");
+let myWorker2 = new Worker("./worker.js?v=6.1.13");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -5787,7 +5791,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.1.12");
+    myWorker2 = new Worker("./worker.js?v=6.1.13");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], mid === manualAreasOnly, tempSections, settings['optOutSections']]);
     workerOut++;
