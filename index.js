ce73f5d4f9685a92ddd9e8e7a4fd07542870fae7
source chunk <duosource.iron@gmail.com>
2024-06-26 13:12:40 -0600

    6.2.24 - Fixed taskUnlocks primary item drop bug

--- index.js
+++ index.js
@@ -1315,7 +1315,7 @@ let expandChallengeStr = '';
 let detailsStack = [];
 let touchTime = 0;
 
-let currentVersion = '6.2.23';
+let currentVersion = '6.2.24';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1440,7 +1440,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "runescape_world_map.png?v=6.2.23";
+mapImg.src = "runescape_world_map.png?v=6.2.24";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3076,7 +3076,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.2.23");
+        myWorker = new Worker("./worker.js?v=6.2.24");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
         workerOut = 1;
@@ -3359,8 +3359,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.2.23");
-let myWorker2 = new Worker("./worker.js?v=6.2.23");
+let myWorker = new Worker("./worker.js?v=6.2.24");
+let myWorker2 = new Worker("./worker.js?v=6.2.24");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -4891,6 +4891,7 @@ let enableTestMode = function(close, fromConfirm) {
         (viewOnly || inEntry || locked) && $('.center').css('margin-top', '0px');
         (viewOnly || inEntry || locked) && $('.pick, .roll2, .unpick').css('opacity', 0).hide();
         (viewOnly || inEntry || locked) && $(`.backlogSources-container`).remove();
+        chunkTasksOn && setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.']);
     } else {
         unlockChallenges();
         $('.open-manual-outer-container').css('opacity', 1).show();
@@ -6060,7 +6061,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.2.23");
+    myWorker2 = new Worker("./worker.js?v=6.2.24");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
     workerOut++;
@@ -9702,11 +9703,13 @@ let showChunkHistory = function() {
 // Selects correct active context menu item
 let switchActiveContext = function(e, opt) {
     activeContextMenuOpen = false;
-    switch (opt) {
-        case "backlog": backlogChallenge(activeContextMenuChallenge, activeContextMenuSkill, '', e.altKey); break;
-        case "backlog note": showNotes(encodeRFC5987ValueChars(activeContextMenuChallenge), activeContextMenuSkill, ''); break;
-        case "alternatives": showAlternatives(activeContextMenuChallenge, activeContextMenuSkill, ''); break;
-        case "details": showDetails(encodeRFC5987ValueChars(activeContextMenuChallenge), activeContextMenuSkill, ''); break;
+    if (!(viewOnly || inEntry || locked) || testMode) {
+        switch (opt) {
+            case "backlog": backlogChallenge(activeContextMenuChallenge, activeContextMenuSkill, '', e.altKey); break;
+            case "backlog note": showNotes(encodeRFC5987ValueChars(activeContextMenuChallenge), activeContextMenuSkill, ''); break;
+            case "alternatives": showAlternatives(activeContextMenuChallenge, activeContextMenuSkill, ''); break;
+            case "details": showDetails(encodeRFC5987ValueChars(activeContextMenuChallenge), activeContextMenuSkill, ''); break;
+        }
     }
     $(".active-context-menu").hide(100);
 }
@@ -9714,10 +9717,12 @@ let switchActiveContext = function(e, opt) {
 // Selects correct backlog context menu item
 let switchBacklogContext = function(opt) {
     backlogContextMenuOpen = false;
-    switch (opt) {
-        case "unbacklog": unbacklogChallenge(backlogContextMenuChallenge, backlogContextMenuSkill); break;
-        case "edit note": showNotes(backlogContextMenuChallenge, backlogContextMenuSkill, backlog[backlogContextMenuSkill][backlogContextMenuChallenge]); break;
-        case "details": showDetails(encodeRFC5987ValueChars(backlogContextMenuChallenge), backlogContextMenuSkill, ''); break;
+    if (!(viewOnly || inEntry || locked) || testMode) {
+        switch (opt) {
+            case "unbacklog": unbacklogChallenge(backlogContextMenuChallenge, backlogContextMenuSkill); break;
+            case "edit note": showNotes(backlogContextMenuChallenge, backlogContextMenuSkill, backlog[backlogContextMenuSkill][backlogContextMenuChallenge]); break;
+            case "details": showDetails(encodeRFC5987ValueChars(backlogContextMenuChallenge), backlogContextMenuSkill, ''); break;
+        }
     }
     $(".backlog-context-menu").hide(100);
 }
