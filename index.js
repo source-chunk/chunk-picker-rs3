9385475d9a9f5c822d3d6e09f19b217f6888ea8b
source chunk <duosource.iron@gmail.com>
2024-11-15 23:41:32 -0700

    6.5.15 - 404 page

--- index.js
+++ index.js
@@ -1388,7 +1388,7 @@ let topbarElements = {
     'Sandbox Mode': `<div><span class='noscroll' onclick="enableTestMode()"><i class="gosandbox fas fa-flask" title='Sandbox Mode'></i></span></div>`,
 };
 
-let currentVersion = '6.5.14';
+let currentVersion = '6.5.15';
 let patchNotesVersion = '6.4.0';
 
 // Patreon Test Server Data
@@ -1529,7 +1529,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "runescape_world_map.png?v=6.5.14";
+mapImg.src = "runescape_world_map.png?v=6.5.15";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3211,7 +3211,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.5.14");
+        myWorker = new Worker("./worker.js?v=6.5.15");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary]);
         workerOut = 1;
@@ -3514,8 +3514,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.5.14");
-let myWorker2 = new Worker("./worker.js?v=6.5.14");
+let myWorker = new Worker("./worker.js?v=6.5.15");
+let myWorker2 = new Worker("./worker.js?v=6.5.15");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -4147,9 +4147,9 @@ let exportFunc = function(type) {
         navigator.clipboard.writeText(tasksTemp);
     } else if (type === 'rules') {
         navigator.clipboard.writeText(JSON.stringify(rules));
-    } else if (type === 'plugin') {
+    } else if (type === 'plugin' && (testMode || !(viewOnly || inEntry || locked))) {
         calcCurrentChallengesCanvas(true, true);
-    } else if (type === 'assign') {
+    } else if (type === 'assign' && (testMode || !(viewOnly || inEntry || locked))) {
         selectAllNeighborsCanvas();
     }
     closeClipboard();
@@ -6301,7 +6301,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.5.14");
+    myWorker2 = new Worker("./worker.js?v=6.5.15");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary]);
     workerOut++;
@@ -7869,6 +7869,7 @@ let openClipboard = function() {
     $('.settings-menu').hide();
     $('.settings').css({ 'color': 'var(--colorText)' });
     modalOutsideTime = Date.now();
+    $('#plugin-clipboard-button, #assign-clipboard-button').prop('disabled', !(testMode || !(viewOnly || inEntry || locked)));
 }
 
 // Opens the search within my chunks modal
@@ -10972,6 +10973,15 @@ let checkMID = function(mid) {
         onMobile && $('#patch-menu').addClass('mobile');
         $('#patch-menu').show();
         $('html, body').addClass('patch');
+    } else if (mid === '404') {
+        atHome = true;
+        $('.loading, .ui-loader-header').remove();
+        $('.menu, .menu2, .menu3, .menu4, .menu5, .menu6, .menu7, .menu8, .menu9, .menu10, .menu11, .settings-menu, .topnav, #beta, .hiddenInfo, #entry-menu, #highscore-menu, #highscore-menu2, #import-menu, #help-menu, .canvasDiv, .gomobiletasks, .menu12, .menu13, .menu14').hide();
+        $('#home-menu, .entry-home-menu-container, .entry-home-menu-extra').hide();
+        onMobile && $('#a404-menu').addClass('mobile');
+        $('#a404-menu').show();
+        $('html, body').addClass('a404');
+        $('.a404-address').text(window.location.href.split('?')[1]);
     } else if (mid) {
         if (mid.split('-')[1] === 'view') {
             mid = mid.split('-')[0];
@@ -10997,11 +11007,14 @@ let checkMID = function(mid) {
                                     if (!!snap4.val()) {
                                         window.location.replace(window.location.href.split('?')[0] + '?' + mid.toLowerCase());
                                     } else {
-                                        window.location.replace(window.location.href.split('?')[0]);
                                         atHome = true;
-                                        $('.menu, .menu2, .menu3, .menu4, .menu5, .menu6, .menu7, .menu8, .menu9, .menu10, .menu11, .settings-menu, .topnav, #beta, .hiddenInfo, #entry-menu, #highscore-menu, #highscore-menu2, #import-menu, #help-menu, .canvasDiv, .gomobiletasks, .menu12, .menu13, .menu14').hide();
                                         $('.loading, .ui-loader-header').remove();
-                                        $('html, body').addClass('home');
+                                        $('.menu, .menu2, .menu3, .menu4, .menu5, .menu6, .menu7, .menu8, .menu9, .menu10, .menu11, .settings-menu, .topnav, #beta, .hiddenInfo, #entry-menu, #highscore-menu, #highscore-menu2, #import-menu, #help-menu, .canvasDiv, .gomobiletasks, .menu12, .menu13, .menu14').hide();
+                                        $('#home-menu, .entry-home-menu-container, .entry-home-menu-extra').hide();
+                                        onMobile && $('#a404-menu').addClass('mobile');
+                                        $('#a404-menu').show();
+                                        $('html, body').addClass('a404');
+                                        $('.a404-address').text(window.location.href.split('?')[1]);
                                     }
                                 });
                             }
