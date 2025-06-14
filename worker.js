93de2cf3596984eae754e3701ef99ec078d371d8
Snowy <devsnowgames@gmail.com>
2025-06-14 16:27:18 +0200

    Reported fixes

--- worker.js
+++ worker.js
@@ -3339,7 +3339,7 @@ let calcChallengesWork = function(chunks, baseChunkData, oldTempItemSkill) {
                     return true;
                 }
             });
-			if (!rules['Cleaning Herbs'] && (name.includes('Clean a') || name.includes('(unf)')) && skill === 'Herblore' && chunkInfo['challenges'][skill][name]['Level'] > 1) {
+			if (!rules['Cleaning Herbs'] && (name.includes('Clean a') || name.includes('(unf)') || name.includes('(unfinished)')) && skill === 'Herblore' && chunkInfo['challenges'][skill][name]['Level'] > 1) {
                 chunkInfo['challenges'][skill][name]['NeverShow'] = true;
             }
             if (wrongThings.length > 0) {
