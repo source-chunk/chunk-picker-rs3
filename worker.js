e47979d32f7cf847e74c31700554a6a0264aae0c
Snowy <devsnowgames@gmail.com>
2026-04-08 18:11:10 +0200

    fixes

--- worker.js
+++ worker.js
@@ -2830,7 +2830,7 @@ let calcChallenges = function(chunks, baseChunkData) {
                 'Archaeology': 'Archie',
                 'Attack': 'Sifu',
                 'Constitution': 'Morty',
-                'Construction': 'Baby Yaga%27s house',
+                'Construction': "Baby Yaga's House",
                 'Cooking': 'Ramsay',
                 'Crafting': 'Gemi',
                 'Defence': 'Wallace',
