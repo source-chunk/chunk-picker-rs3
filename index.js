3152257a701fcb0fdaf1f71be96dda2d1c285380
Snowy <devsnowgames@gmail.com>
2026-05-30 23:20:17 +0200

    Passives and specs

--- index.js
+++ index.js
@@ -1598,7 +1598,7 @@ let topbarElements = {
 
 let currentVersion = '6.9.48';
 let currentEnforcedVersion = '6.9.45';
-let patchNotesVersion = '6.9.8.2';
+let patchNotesVersion = '6.9.48';
 let updateLevel = 'maintenance-mode';
 
 // Patreon Test Server Data
